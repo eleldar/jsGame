51185e92a031a89604453533421c0eb4eda50e4d
Eldar <eleldar@mail.ru>
2021-04-07 18:52:01 +0300

    task #1

--- main.js
+++ main.js
@@ -17,3 +17,27 @@ const player2 = {
         console.log(this.name + ' Fight...');
     }
 };
+
+function createPlayer() {
+    const $player1 = document.createElement('div');
+    $player1.classList.add('player1');
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $player1.appendChild($progressbar);
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+    const $img = document.createElement('img');
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+    $character.appendChild($img);
+    $player1.appendChild($character);
+    const $arenas = document.querySelector('.arenas');
+    $arenas.appendChild($player1);
+    $life.style.width = '100%';
+    $name.innerText = 'SCORPION';
+};
