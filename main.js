1ba50afa35902a2bf18bfb088159129ceb4cd7bf
eleldar <eleldar@mail.ru>
2021-04-10 22:21:42 +0300

    additional functions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
 const player1 = {
     player: 1,
@@ -54,6 +55,27 @@ function createPlayer(actor) {
 
     return $player;
 };
+
+function changeHP(player) {
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+    player.hp -= 20;
+    $playerLife.style.width = player.hp + '%';
+
+    if (player.hp < 0) {
+        $arenas.appendChild(playerLose(player.name))
+    }
+}
+
+function playerLose(name) {
+    const $loseTitle = createElement('div', 'loseTitle')
+    $loseTitle.innerText = name + 'lose';
+    return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function () {
+    changeHP(player1);
+    changeHP(player2);
+    })
 
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
